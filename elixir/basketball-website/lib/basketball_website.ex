dc45c3c7392a178acba2743702c8a1242a2e757f
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-02 18:20:37 +0000

    complete basketball-website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,12 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    [key | tail] = String.split(path, ".", parts: 2)
+
+    case tail do
+      [] -> data[key]
+      [next_path] -> extract_from_path(data[key], next_path)
+    end
   end
 
-  def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
-  end
+  def get_in_path(data, path), do: get_in(data, String.split(path, "."))
 end
