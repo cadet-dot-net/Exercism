b33d7eb698cd843e293f6c2edb76050408b2de64
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-04 01:12:37 +0000

    ts complete resistor-duo-test

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,16 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+import { inspect } from "util"
+let colours: Array<string> = 
+  ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+export function decodedValue(codes: Array<string>) {
+  // throw new Error('Remove this statement and implement this function')
+  // for (var code of codes) {
+  //   console.log(colours.indexOf(code))
+  // }
+  // (10).toString()
+  var code = 
+    colours.indexOf(codes[0]) * 10 + 
+    colours.indexOf(codes[1])
+    
+  return code;
 }
