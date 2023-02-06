a5c26fc08ecec9d7d6eb696d8be4b061cf82e665
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-06 13:10:00 +0000

    ts complete resistor-color

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,16 @@
+export const colorCode = (band: string): number => {
+  return COLORS.indexOf(band)
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+];
