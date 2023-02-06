8af02062601b2aa638ede1c5b3b6e79b03b32ee5
tracy-o <113360074+tracy-o@users.noreply.github.com>
2023-02-06 11:50:27 +0000

    ts complete resistor-color-trio

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,22 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const colours = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+]
+
+export function decodedResistorValue(bands: Array<string>): string {
+  var code = colours.indexOf(bands[0]) * 10 +  colours.indexOf(bands[1])
+  var exponent = colours.indexOf(bands[2])
+  if (exponent >= 2) {
+    return code * (10 ** (exponent - 3)) +" kiloohms"
+  } else {
+    return code * (10 ** exponent)+" ohms"
+  }
 }
