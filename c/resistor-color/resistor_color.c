d46a8863aac43c0a3c4a223210866c7815bd412d
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2024-11-25 19:46:23 +0000

    [c] resistor-color

---
+++ c/resistor-color/resistor_color.c
@@ -0,0 +1,22 @@
+#include "resistor_color.h"
+
+int color_code(resistor_band_t band) {
+    return band;
+}
+
+const resistor_band_t* colors(void) {
+    static resistor_band_t bands[] = {
+        BLACK,
+        BROWN,
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        BLUE,
+        VIOLET,
+        GREY,
+        WHITE
+    };
+    
+    return bands;
+}
